7a8b02b73fdbe7986f15ba94d23dcb0b412b346a
Felipe Americano <felip.americano@gmail.com>
2020-04-23 21:10:00 -0300

    perdi a manha no front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Light London',
+  address: '10  London W9 1DT',
+  description: 'A lovely summer  spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 7,
+  number_of_guests: 35
+)
+Flat.create!(
+  name: 'Light & Spacious don',
+  address: '10 Clifton Gn W9 1DT',
+  description: 'A lovelyious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 31
+  )
+
+Flat.create!(
+  name: 'arden Flat London',
+  address: '10 W9 1DT',
+  description: 'flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 7,
+  number_of_guests: 1
+  )
+
+Flat.create!(
+  name: 'Spacious Garden',
+  address: 'Clifton Gardens',
+  description: 'summer garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 8
+  )
+
